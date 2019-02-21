442f3397b9b525b0d2be5ca11eeb2722b85f15ce
Coursera Learner <nobody@nowhere.nul>
2019-02-21 20:09:15 +0000

    assignment complete

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,104 @@
 
 
 void assert_card_valid(card_t c) {
-
-
-}
+  if (c.value < 2 || c.value >14){
+    //    printf("this is not an acceptable card value");
+    assert (c.value>=2 && c.value<=14);
+      }
+  if (c.suit < SPADES || c.suit >CLUBS){
+    //    printf("this is not an acceptable card suit");
+    assert (c.suit>=SPADES && c.suit<=CLUBS);
+      }
+  }
 
 const char * ranking_to_string(hand_ranking_t r) {
+  switch(r){
+  case 0: return "STRAIGHT_FLUSH"; break;
+  case 1:  return "FOUR_OF_A_KIND"; break; 
+  case 2:  return "FULL_HOUSE"; break; 
+  case 3: return "FLUSH"; break;
+  case 4: return "STRAIGHT"; break;
+  case 5: return "THREE_OF_A_KIND"; break;
+  case 6: return "TWO_PAIR"; break;
+  case 7: return "PAIR"; break;
+  case 8: return "NOTHING"; break;
+   }
   return "";
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  switch(c.value){
+  case 2: return 50; break;
+  case 3: return 51; break;
+  case 4: return 52; break;
+  case 5: return 53; break;
+  case 6: return 54; break;
+  case 7: return 55; break;
+  case 8: return 56; break;
+  case 9: return 57; break;
+  case 10: return 48; break;
+  case 11: return 74; break;
+  case 12: return 81; break;
+  case 13: return 75; break;
+  case 14: return 65; break;    
+}
+    return 'x';
 }
 
-
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit){
+  case 0: return 's' ; break;
+  case 1: return 'h' ; break;
+  case 2: return 'd' ; break;
+  case 3: return 'c' ; break;
+  case 4: return '4'; break;    
+  }
+    return 'x';
 }
 
 void print_card(card_t c) {
-
+  printf("%c%c",value_letter(c),suit_letter(c));
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  int x=value_let;
+  int y=suit_let;
+  switch(x){
+  case 48: temp.value=10;break;
+  case 50: temp.value=2;break;
+  case 51: temp.value=3;break;    
+  case 52: temp.value=4;break;
+  case 53: temp.value=5;break;
+  case 54: temp.value=6;break;
+  case 55: temp.value=7;break;
+  case 56: temp.value=8;break;
+  case 57: temp.value=9;break;
+  case 65: temp.value=14;break;    
+  case 74: temp.value=11;break;
+  case 75: temp.value=13;break;
+  case 81: temp.value=12;break;
+  default:  printf("invalid card letter, cards_from_letters failed \n ");
+      exit(EXIT_FAILURE);
+  }
+  switch (y){
+  case 99: temp.suit=3;break;
+  case 100: temp.suit=2;break;
+  case 104: temp.suit=1;break;       
+  case 115: temp.suit=0;break;
+  default:  printf("invalid card suit, cards_from_letters failed \n ");
+      exit(EXIT_FAILURE);
+  }
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  temp.value=c%13+2;
+  temp.suit=(c/13)%4;
+  if (c>51){
+    printf("invalid: numbers should be between 0 and 51 for a 52-card deck ");
+      exit(EXIT_FAILURE);
+  }
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,52 @@
 #include "cards.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void) {
-
+  card_t c;
+  //   char c3[20];
+  //  char * c2;
+   hand_ranking_t myRank = STRAIGHT_FLUSH;
+    int n;
+    int n1;
+    //  printf("input 2-14 for your card value ");
+    //    fgets(c3,14,stdin);
+    //  n=atoi(c3);
+    //  printf("input an integer for your suit (0-3)SPADES, HEARTS, DIAMONDS, CLUBS) ");
+    //   fgets(c2,20,stdin);
+    //  printf ("string is %s \n", c2);
+    //  n1=atoi(c2);
+    //  fgets (n,1024,stdin);
+      for (n1=0;n1<4; n1++) {
+	printf("letter for card and suit ");
+    for (n=2;n<15; n++) {  
+      c.value=n;
+  c.suit=n1;
+  assert_card_valid(c);
+  //  char c1=suit_letter(c);
+  //char c4=value_letter(c);
+  print_card(c);
+  // printf(" %c%c ",c4,c1);
+    }
+  printf("\n");    
+}
+      printf("ranking case 0 to 9 ");
+          for (myRank=0; myRank<10; myRank++) {
+      printf("%s ",  ranking_to_string(myRank));
+      }
+	  printf("\n");
+	  printf("card from letters \n");
+	  c=card_from_letters('Q','c');
+	  print_card(c);
+     printf("\n");
+     for (unsigned j=0; j<52; j++) {
+       c=card_from_num(j);
+       print_card(c);
+       if (j==12||j==25||j==38||j==51){
+	 printf("\n");
+       }
+       
+     }
+     printf("end of main \n");
+     return 0;
 }
